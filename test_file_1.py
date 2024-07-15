dea6a467e449e1172118072201f8785d9aafc17d
Rustam Mustafaev <115871483+RustamMust@users.noreply.github.com>
2024-07-15 17:55:16 +0500

    Update test_file_1.py

--- test_file_1.py
+++ test_file_1.py
@@ -5,21 +5,4 @@ class OneFile:
 class SecondFile(OneFile):
     pass
 
-class OneFileThatNeed:
-    pass
-
-class FactoryFile:
-    pass
-
-
-class FactoryNotFile:
-    pass
-
-
-b = FactoryFile()
-ability = FactoryNotFile()
-
-print('hello world')
-print('i am not so busy')
-print('new factory')
 new_value = 'value string'
