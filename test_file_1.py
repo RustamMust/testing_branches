485c445ce461775d239c3f9c9332ab4b040f006e
RustamMust <rustammustavaev@gmail.com>
2024-07-15 17:37:43 +0500

    adding 'new value string'
    (cherry picked from commit b8bb9c152879f466e7e8542aac0f3818a8b4a111)
    
    # Conflicts:
    #	test_file_1.py

--- test_file_1.py
+++ test_file_1.py
@@ -9,3 +9,13 @@ new_value = 'value string'
 
 new_value_2 = 'value string 2 NEW VALUE'
 
+class FactoryNotFile:
+    pass
+
+
+b = FactoryFile()
+ability = FactoryNotFile()
+
+print('hello world')
+print('i am not so busy')
+print('new factory')
